a06dc5017a92093c119e7df9376072030537b535
amitrayblr <50335608+amitrayblr@users.noreply.github.com>
2020-05-20 20:11:14 +0530

    Fixed errors

--- cachematrix.R
+++ cachematrix.R
@@ -10,15 +10,13 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() {
     x
   }
-  setmean <- function(inverse) {
+  setinverse <- function(inverse) {
     i <<- inverse
   }
-  getmean <- function() {
+  getinverse <- function() {
     i
   }
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
